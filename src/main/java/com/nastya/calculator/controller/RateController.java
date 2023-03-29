8954fd0395068c861eaf63ea1c711dbc324ffd9d
agavriluta <agavriluta@griddynamics.com>
2023-03-29 10:20:32 +0300

    1

---
+++ src/main/java/com/nastya/calculator/controller/RateController.java
@@ -0,0 +1,35 @@
+package com.nastya.calculator.controller;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+public class RateController {
+//    private static final String template = "Hello,%s!";
+//
+//    private final PlayerService playerService;
+//    private final AtomicLong counter = new AtomicLong();
+//
+//    @GetMapping("/greeting")
+//    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+//    }
+//    @GetMapping("/anotherGreeting")
+//    public String anotherGreeting() {
+//        return "Hello";
+//    }
+//
+//    @PostMapping("/volleyball-player")
+//    public String addPlayer(@RequestBody VolleyballPlayer volleyballPlayer) {
+//        playerService.players.add(volleyballPlayer);
+//
+//        return String.valueOf(playerService.players.size());
+//    }
+
+
+
+
+
+}
+
