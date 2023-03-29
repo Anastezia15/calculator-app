bf957b0d6af9939fe8760e3b7fb3cfbdacc9f98b
pw03153 <85jaayZsT57uXAV15Vh1>
2023-03-29 11:30:57 +0300

    add database support

---
+++ src/main/java/com/nastya/calculator/model/Bank.java
@@ -0,0 +1,46 @@
+package com.nastya.calculator.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import org.hibernate.Hibernate;
+
+import java.util.Objects;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Bank {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    private String name;
+
+    private String address;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        Bank bank = (Bank) o;
+        return id != null && Objects.equals(id, bank.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return getClass().hashCode();
+    }
+}
